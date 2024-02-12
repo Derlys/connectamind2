468314ddb69ebe66afac70c0b15a84e7f7c61b8c
Derlys Dominguez <derlys275@gmail.com>
2024-02-12 22:18:20 +0000

    feat: connect wallet with wallet adapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'connectamind-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `<header class="px-16 pt-24 pb-08">
+      <h1 class="text-center text-5xl mb-4">My bank Connect a Mind</h1>
+      <div class="flex justify-center">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
+    </header>
+    <main></main>`,
 })
-export class AppComponent {
-  title = 'connectamind';
-}
+export class AppComponent {}
