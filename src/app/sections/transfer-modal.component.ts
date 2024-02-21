92aa011632f38ec8e2efe7169d09f959bbb146b7
Derlys Dominguez <derlys275@gmail.com>
2024-02-21 16:08:24 +0000

    feat:method of submit form

--- src/app/sections/transfer-modal.component.ts
+++ src/app/sections/transfer-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TransferFormComponent } from '../pages/transfer-form.component';
+import { TransferFormPayLoad } from '../models/transfer-form-payload';
 
 @Component({
   standalone: true,
@@ -7,8 +8,13 @@ import { TransferFormComponent } from '../pages/transfer-form.component';
   selector: 'connectamind-transfer-modal',
   template: `<div class="px-4 pb-8 pt-16 bg-orange-600">
     <h3 class="text-3xl text-center mb-8">TRANFER FOUNDS</h3>
-    <connectamind-transfer-form></connectamind-transfer-form>
+    <connectamind-transfer-form
+      (submitForm)="onTransfer($event)"
+    ></connectamind-transfer-form>
   </div>`,
 })
-export class TransferModalComponent {}
-q;
+export class TransferModalComponent {
+  onTransfer(payload: TransferFormPayLoad) {
+    console.log('hello world', payload);
+  }
+}
