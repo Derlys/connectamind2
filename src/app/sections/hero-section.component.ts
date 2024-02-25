1c30c5c275bc3d714bc181c96c062df1d012e507
Derlys Dominguez <derlys275@gmail.com>
2024-02-25 18:28:30 +0000

    fix: changes in responsive

--- src/app/sections/hero-section.component.ts
+++ src/app/sections/hero-section.component.ts
@@ -89,7 +89,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
           <!-- Background backdrop, show/hide based on slide-over state. -->
           <div class="fixed inset-0 z-50"></div>
           <div
-            class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-orange-600 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+            class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
           >
             <div class="flex items-center justify-between">
               <a href="/" class="-m-1.5 p-1.5">
@@ -125,23 +125,23 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
               <div class="-my-6 divide-y divide-gray-500/10">
                 <div class="space-y-2 py-6">
                   <a
-                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-600 hover:bg-orange-500"
                     routerLinkActive="active"
                     [routerLink]="['']"
                     (click)="toggle()"
                     >Home</a
                   >
                   <a
-                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-600 hover:bg-orange-500"
                     routerLinkActive="active"
                     [routerLink]="['settings']"
                     (click)="toggle()"
                     >Settings</a
                   >
                   <a
-                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-600 hover:bg-orange-500"
                     routerLinkActive="active"
-                    routerLink="/contact"
+                    [routerLink]="['balance']"
                     (click)="toggle()"
                     >Balance</a
                   >
